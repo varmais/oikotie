a8f80e94788666a2e56825728a26e9c7cac36191
VarmaIS <teemu.tiilikainen@gmail.com>
2014-11-22 14:55:51 +0200

    Take arguments, write data, fixed couple of bugs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 oikotie
 =======
+
+Scrape listing data from oikotie.fi. Needs card.json file from search results page as an input.
+
+Usage:
+
+    node oiko.js <json input> <csv output>
+
+ie:
+
+    node oiko.js card.json data.csv
--- oiko.js
+++ oiko.js
@@ -1,16 +1,53 @@
 var fs 		= require('fs'),
 	request = require('request'),
-	cheerio = require('cheerio');
+	cheerio = require('cheerio'),
+	csv		= require('fast-csv'),
+	args	= process.argv.slice(2);
 
 
 var ids 		= [],
 	listings 	= [],
 	i 			= 0,
-	jsonFile 	= 'card.json',
-	baseUrl 	= 'http://asunnot.oikotie.fi/myytavat-asunnot/';
+	reqDelay	= 1000,
+	jsonFile 	= args[0],
+	baseUrl 	= 'http://asunnot.oikotie.fi/myytavat-asunnot/'
+	dataFile	= args[1];
+
+function getListingData(id) {
+	setTimeout(function() {
+
+		request(baseUrl + id, function(err, res, body) {
+
+			console.log("Requesting listing " + (i+1) + " of " + ids.length);
+
+			var $ = cheerio.load(body),
+			listing = {};
+
+			$('.widget-card-information-table tr').each(function(i, el) {
+				var prop = $(this).find('th').text().trim(),
+				val = $(this).find('td').text().trim().replace('\n', ' ');
+
+				listing[prop] = val;
+			});
+
+			listings.push(listing);
+
+			// test purposes
+			//if (i < 3) {
+			if (i < ids.length) {
+				i++;
+				getListingData(ids[i]);
+			} else {
+				csv.writeToPath(dataFile, listings, {headers: true}).on('finish', function() {
+					console.log('Done writing data. Check ' + dataFile);
+				});
+			}
+		});
+	}, reqDelay);
+}
 
 fs.readFile(jsonFile, 'utf-8', function(err, data) {
-	
+
 	if (err) {
 		return console.error(err);
 	}
@@ -20,34 +57,6 @@ fs.readFile(jsonFile, 'utf-8', function(err, data) {
 	json.response.docs.forEach(function(card) {
 		ids.push(card.card_id);
 	});
-
-	function getListingData(id) {
-		setTimeout(function() {
-			
-			request(baseUrl + id, function(err, res, body) {
-				
-				var $ = cheerio.load(body),
-					listing = {};
-				
-				$('.widget-card-information-table tr').each(function(i, el) {
-					var prop = $(this).find('th').text().trim(),
-						val = $(this).find('td').text().trim();
-					
-					listing[prop] = val;
-					listings.push(listing);
-				});
-			});
-
-			
-			// test purposes
-			if (i < 3) {
-			// if (i < ids.length) {
-				i++;
-				getListingData(ids[i]);	
-			}
-			
-		}, 1000);
-	}
 
 	// init loop
 	getListingData(ids[i]);
